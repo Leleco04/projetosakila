7096797649dac8bf287b4fae111d716886b446b5
Leandro Hideki <166633513+Leleco04@users.noreply.github.com>
2024-08-19 14:33:59 -0300

    Add files via upload

--- sakila.sql
+++ sakila.sql
@@ -248,18 +248,18 @@ DROP TABLE IF EXISTS `film`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `film` (
   `film_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
   `description` text DEFAULT NULL,
-  `release_year` year(4) DEFAULT NULL,
-  `language_id` tinyint(3) unsigned NOT NULL,
+  `release_year` int(11) DEFAULT NULL,
+  `language_id` tinyint(3) unsigned DEFAULT NULL,
   `original_language_id` tinyint(3) unsigned DEFAULT NULL,
-  `rental_duration` tinyint(3) unsigned NOT NULL DEFAULT 3,
-  `rental_rate` decimal(4,2) NOT NULL DEFAULT 4.99,
+  `rental_duration` tinyint(3) unsigned DEFAULT 3,
+  `rental_rate` decimal(4,2) DEFAULT 4.99,
   `length` smallint(5) unsigned DEFAULT NULL,
-  `replacement_cost` decimal(5,2) NOT NULL DEFAULT 19.99,
+  `replacement_cost` decimal(5,2) DEFAULT 19.99,
   `rating` enum('G','PG','PG-13','R','NC-17') DEFAULT 'G',
   `special_features` set('Trailers','Commentaries','Deleted Scenes','Behind the Scenes') DEFAULT NULL,
-  `last_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `last_update` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`film_id`),
   KEY `idx_title` (`title`),
   KEY `idx_fk_language_id` (`language_id`),
@@ -762,4 +762,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-08-19 14:18:28
+-- Dump completed on 2024-08-19 14:33:31
